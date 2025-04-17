54437cafe9ceee4f7a6f0f5ef11be1ede389d9fc
ekain <eregil001@ikasle.ehu.eus>
2025-04-18 00:00:15 +0200

    mate.c, etc.

--- include/game.h
+++ include/game.h
@@ -4,17 +4,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-/*
+#include "mate.h"
 
-
-
-*/
-
-// maparen goiko ezkerreko iskinatik kontatuta
-typedef struct  {
-    int x;
-    int y;
-} koord_t;
 
 typedef struct {
     // erlatiboak
@@ -24,25 +15,11 @@ typedef struct {
     uint8_t abiadura;
 } etsaia_t;
 
-// Norabide bektore normalizatua borobilduta. Balio posibleak: -1, 0, 1
-typedef struct {
-    char x;
-    char y;
-} int_norabide_t;
-
 etsaia_t etsaia_hasieratu();
-//double modulua(koord_t k);
-int distantzia(koord_t a, koord_t b);
 void tick();
 void marraztu();
 void spawnEnemy();
-koord_t rel2abs(koord_t rel);
-int random_int(int min, int max);
-koord_t rel2pant(koord_t rel);
-koord_t abs2pant(koord_t abs);
-koord_t abs2rel(koord_t abs);
 bool pantailan_dago(koord_t rel);
-int_norabide_t lortu_norabidea(koord_t rel);
 
 // Aldagai globalak
 extern int EGOERA;
---
+++ include/mate.h
@@ -0,0 +1,38 @@
+#ifndef MATE_H
+#define MATE_H
+
+/* ****** mate.h ********
+Hemen orotariko funtzio eta datu mota matematikoak definitzen dira; hala nola, 
+koordenatuak eta erreferentzi sistemen artean pasatzeko funtzioak.
+
+abs: koordenatu absolutuak: maparen koordenatuak. Zentrua 0,0-n dago eta limiteak {...}-n definituta daude. [TODO: bermatu, uste dot orain ez dela hala]
+rel: jokalarian zentratuta: pantailaren zentrua -- hau da, pantailan jokalariaren posizioa, hau beti erdian egongo baita -- da 0,0 puntua.
+pant: pantailaren koordenatuak: pantailaren lehenengo pixela, goiko ezkerreko iskinakoa, da 0,0 puntua, eta ez da onartzen balio negatiborik.
+
+Denak koord_t dira, beraz, kontuz parametroen izenekin, horiek esango baitute zein motatakoak izan behar diren koordenatuak. Batetik bestera 
+pasatzeko [a]2[b] motako funtzioak daude erabilgarri.
+*/
+
+typedef struct  {
+    int x;
+    int y;
+} koord_t;
+
+// Norabide bektore normalizatua borobilduta. Balio posibleak: -1, 0, 1
+typedef struct {
+    char x;
+    char y;
+} int_norabide_t;
+
+int distantzia(koord_t a, koord_t b);
+koord_t rel2abs(koord_t rel);
+int random_int(int min, int max);
+koord_t rel2pant(koord_t rel);
+koord_t abs2pant(koord_t abs);
+koord_t abs2rel(koord_t abs);
+int_norabide_t lortu_norabidea(koord_t rel);
+float fast_sqrt(float x);
+// Bektorearen modulua.
+float modulua(koord_t k);
+
+#endif
